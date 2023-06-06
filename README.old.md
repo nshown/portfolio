f58214402051466acf1f8ff1b0b258d4d0efd705
Nathan Shown <nshown@instructors.2u.com>
2023-06-06 19:05:59 -0400

    added main page images and styling

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# portfolio
-Portfolio and resume website.
--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import eventHorizonImg from './assets/img/eventhorizon2.jpg';
 
 function App() {
   return (
     <div className="App">
+      <img src={eventHorizonImg} class="corner-img" alt="" />
       <header className="App-header">
         <p>
           Add webpage here!
