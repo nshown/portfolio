5b60325180496670df71847a794b829474da619e
Nathan Shown <nshown@instructors.2u.com>
2023-06-06 23:48:31 -0400

    Updated images and background

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import eventHorizonImg from './assets/img/eventhorizon2.jpg';
+import ribbonWeb from './assets/img/ribbon-web.png';
+
 
 function App() {
   return (
     <div className="App">
-      <img src={eventHorizonImg} class="corner-img" alt="" />
+      <img src={eventHorizonImg} class="top-corner-img" alt="event horizon" />
       <header className="App-header">
         <p>
           Add webpage here!
         </p>
       </header>
+      <img src={ribbonWeb} class="bottom-corner-img" alt="ribbon web"></img>
     </div>
   );
 }
