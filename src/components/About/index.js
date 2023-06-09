3cb48153cfef4ec4432bf75bd09921e9ac665cc3
Nathan Shown <nshown@instructors.2u.com>
2023-06-09 18:36:12 -0400

    Added resume data to about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,19 +1,42 @@
 import React from 'react';
+import './About.css';
 
 function About() {
   return (
-    <section className="my-5">
-      <div className="my-2">
-        <div className="profile-img my-5">
-        <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
-        </div>
-        <p>
-        Helvetica put a bird on it church-key Blue Bottle banjo bespoke brunch Etsy authentic Marfa quinoa typewriter plaid direct trade small batch wayfarers bicycle rights cliche craft beer gastropub single-origin coffee Godard Carles you probably haven't heard of them irony pickled kitsch synth sriracha gentrify literally heirloom blog Truffaut paleo scenester
-        </p>
-        <p>
-        Echo Park sriracha Pinterest vegan biodiesel 90's irony iPhone Kickstarter Carles crucifix kitsch narwhal dreamcatcher pickled trust fund selvage art party letterpress Tumblr post-ironic kogi Thundercats Tonx Brooklyn Pitchfork Odd Future authentic normcore freegan leggings Schlitz chambray organic tousled retro fap squid street art church-key fashion axe Wes Anderson bespoke whatever gentrify banh mi you probably haven't heard of them American Apparel next level pug ugh.
-        </p>
+    <section>
+      <div>
+        <p>I am passionate about software engineering.  I love the challenge of creating a new application or enhancing an existing application to better serve the userbase.  My appreciation of application engineering is rooted in my love of problem solving and providing users with a new, enjoyable, and useful experience.  I have worked as an engineer for over a decade and I continue to delight in the experience.</p>
       </div>
+      <div>
+        <h1>Skills</h1>
+        <div>
+          <p>Python, JavaScript, Typescript, Node, React, C#, PHP, GraphQL, Java, ML, MongoDB, SQL, HTML5, CSS3, AWS, Azure, Scrum/Agile, MVVM, MVC, OOP, TDD</p>
+        </div>
+      </div>
+      <div>
+        <h1>Experience</h1>
+        <div>
+          <div>
+            <h3>Lead Software Engineer and CEO skyLyn Applications LLC</h3>
+            <h4>skyLyn Applications LLC</h4>
+            <h5>2014 - present</h5>
+            <p>Creating a social network web app focusing on users sharing customizable adventure experiences coded using Node, Typescript, Sequelize, and React.  Using C#, JavaScript, SQL, and HTML5 to produce a web scraping desktop app called ScrapeMate.  Using PHP, SQL, and JavaScript to produce the website to distribute ScrapeMate, receive/track user orders, and educate my userbase.</p>
+          </div>
+          <div>
+            <h3>Data Visualization and Web Development Bootcamp Instructor</h3>
+            <h4>edX</h4>
+            <h5>2020 - present</h5>
+            <p>Instructing students with limited technical backgrounds in Python, Pandas, Matplotlib, Flask, ML, HTML, JavaScript, Node, SQL, React, and more with the intention they become competent entry level web engineers or data analyst.  Creating additional curriculum materials to assist students learning.  One-on-one tutoring students requiring additional assistance.</p>
+          </div>
+          <div>
+            <h3>Lead Software Engineer</h3>
+            <h4>Experis</h4>
+            <h5>2010 - 2014</h5>
+            <p>Used C#, VB, XAML, SQL, and other technologies in Visual Studio to produce Excel addins and standalone applications to store, manipulate, organized and visualize data.  Developed informational materials and hosted instructional sessions to educate users in the proper use of our applications.  Gathered, designed and prioritized project requirements.</p>
+          </div>
+        </div>
+      </div>
+
     </section>
   );
 }
